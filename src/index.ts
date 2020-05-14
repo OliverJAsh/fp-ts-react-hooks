1567b121f5013a4d06e276e0cb0c6d211648605d
Oliver Joseph Ash <oliverjash@gmail.com>
2020-05-14 19:59:02 +0100

    Add support for `memo` HOC and reselect `createSelector`

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import * as A from 'fp-ts/lib/Array';
 import { Eq, fromEquals, strictEqual } from 'fp-ts/lib/Eq';
 import * as O from 'fp-ts/lib/Option';
 import * as ReadonlyArray from 'fp-ts/lib/ReadonlyArray';
+import * as Record from 'fp-ts/lib/Record';
 import * as t from 'io-ts';
 import { option } from 'io-ts-types/lib/option';
 import * as React from 'react';
+import * as reselect from 'reselect';
 import {
     useCustomCompareCallback,
     useCustomCompareEffect,
@@ -28,6 +30,8 @@ const eqArrayDependency = A.getEq(eqDependency);
 
 const eqDependencies = ReadonlyArray.getEq(eqDependency);
 
+const eqProps = Record.getEq(eqDependency);
+
 export const useCallback: typeof React.useCallback = (fn, dependencies) =>
     useCustomCompareCallback(fn, dependencies ?? [], eqDependencies.equals);
 
@@ -36,3 +40,12 @@ export const useEffect: typeof React.useEffect = (fn, dependencies) =>
 
 export const useMemo: typeof React.useMemo = (fn, dependencies) =>
     useCustomCompareMemo(fn, dependencies ?? [], eqDependencies.equals);
+
+export const memo = <P extends object>(
+    Component: React.FC<P>,
+): React.NamedExoticComponent<P> => React.memo(Component, eqProps.equals);
+
+export const createSelector = reselect.createSelectorCreator(
+    reselect.defaultMemoize,
+    eqDependency.equals,
+);
